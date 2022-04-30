0fc917728e3f678c06e9bd5c249ecfca2c98c9c4
ToffaKrtek <krotov.anthon@yandex.ru>
2022-04-30 12:01:58 +0700

    ports

--- src/API.js
+++ src/API.js
@@ -3,7 +3,7 @@ var axios = require('axios');
 
 //PROTOCOL = location.protocol;
 PROTOCOL = "http:";
-URL = "localhost";
+URL = "194.67.74.67";
 //PORT = location.port == 80 ? '' : ":" + location.port;
-PORT = ":8100";
+PORT = ":80";
 axios.defaults.baseURL = PROTOCOL+'//'+ URL + PORT;
